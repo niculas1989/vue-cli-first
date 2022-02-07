4c119e4f7066a4bf15257d2116ee0206face78e6
niculas1989 <nicolas.maranzano@libero.it>
2022-02-07 15:50:41 +0100

    added new file - Hero + p

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <Title />
+    <Hero />
   </div>
 </template>
   
 <script>
 import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
 export default {
-  name: "App",
   components: {
     Title,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>pippo e pluto</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
