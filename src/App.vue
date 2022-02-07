7a673a0463810c4360726682672dbcc7bcba69ff
niculas1989 <nicolas.maranzano@libero.it>
2022-02-07 16:11:50 +0100

    added some comments in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">
+  <!-- App principale in cui importiamo i vari "pezzi" -->
+  <div>
+    <!-- "Title" creato da me ed importato -->
     <Title />
+
+    <!-- "Hero" creato da me ed importato -->
     <Hero />
   </div>
 </template>
   
 <script>
+//| Momento in cui importo i file per poterli utilizzare
 import Title from "./components/Title.vue";
 import Hero from "./components/Hero.vue";
 export default {
@@ -17,4 +22,7 @@ export default {
 </script>
 
 <style>
+/* Eventuale stile per la pagina principale
+Non per forza necessario, visto e considerato che 
+Lavoriamo a "pezzi" */
 </style>
